fac7dbaf1c361240a5df1cf5ab0418571620edda
sg0703 <samgates@Sams-MacBook-Pro.local>
2021-06-15 21:44:03 -0400

    Converted AllPosts to class-based, connected to store

--- client/src/components/AllPosts.js
+++ client/src/components/AllPosts.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const AllPosts = () => {
-    return(
-        <div>
-            <h1>All posts</h1>
-        </div>
-    ); 
+class AllPosts extends React.Component {
+    componentDidMount() {
+        // code in here to fetch all posts from DB
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>All posts</h1>
+            </div>
+        ); 
+    }
 }
 
-export default AllPosts;
+export default connect(null, {})(AllPosts);
