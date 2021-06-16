7ba273524f74b230fee4fadbad5b5710569a439e
sg0703 <samgates@Sams-MacBook-Pro.local>
2021-06-16 13:41:24 -0400

    HCompleted AllPosts, landing page displaying all posts in datbase

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import authReducer from './authReducer';
 import postReducers from './postReducers';
 
 // placeholder
 export default combineReducers ({
-    auth: authReducer,
     posts: postReducers
 });
 
